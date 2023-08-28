4abd892b518a613ad2daf6e3c8704f1c48bf6295
Kate <ekkim94@gmail.com>
2023-08-28 15:52:51 +0900

    inital commit

---
+++ src/main/java/com/habsida/moragoproject/MoragoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.habsida.moragoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoragoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoragoProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/habsida/moragoproject/MoragoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habsida.moragoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoragoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
